1735bb19751a8f951b4eb62771195e5b2220b00f
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-11-05 18:13:12 +0100

    build: set up ESLint, Prettier, Commitlint, lint-staged and Next.js configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "cdn.sanity.io",
+				port: "",
+			},
+		],
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	cacheMaxMemorySize: 0,
+	poweredByHeader: false
+};
 
 export default nextConfig;
