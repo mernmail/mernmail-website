599763c3f2fd732594e596001a7cac88cc64abbb
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-11-07 18:16:40 +0100

    fix: fix the responsiveness of the documentation layout

--- app/docs/layout.jsx
+++ app/docs/layout.jsx
@@ -10,7 +10,9 @@ export default function DocsLayout({ children }) {
         <aside className="hidden md:block md:fixed w-72 px-3 bottom-0 top-12 overflow-y-auto">
           <SidebarLinks links={docLinks} />
         </aside>
-        <main className="flex-1 box-content ml-72 px-2 py-6">{children}</main>
+        <main className="flex-1 box-content md:ml-72 px-2 py-6">
+          {children}
+        </main>
       </div>
     </div>
   );
