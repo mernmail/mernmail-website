c6979c7c33e2888ab5db4c4135f7103febb355ff
Dorian Niemiec <dorian.niemiec@svrjs.org>
2024-11-25 06:24:01 +0100

    style: clean up the root layout code

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -5,27 +5,6 @@ export default function RootLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      {/*<Header
-        docLinks={[
-          {
-            href: "/",
-            target: "_self",
-            label: "Home"
-          },
-          {
-            href: "/docs",
-            target: "_self",
-            label: "Docs",
-            sub: true
-          },
-          {
-            href: "/blog",
-            target: "_self",
-            label: "Blog",
-            sub: true
-          }
-        ]}
-      />*/}
       <div className="flex-grow flex-1 overflow-x-hidden">{children}</div>
       <Footer />
     </div>
